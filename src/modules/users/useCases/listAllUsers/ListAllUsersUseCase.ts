4cf696c55654bffbfaad13d9924bf094efa2dec2
Renato <renato__ferreira@hotmail.com>
2022-06-29 23:28:07 -0300

    challenge finished

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,16 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userRequest = this.usersRepository.findById(user_id);
+
+    if (!userRequest.admin) throw new Error("user is not admin");
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
